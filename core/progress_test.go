a308413ea8b5d12fc938e9ee4946f56ba096c161
Jair Meza <jair3202@gmail.com>
2026-03-23 16:28:48 -0400

    replace manual copy loop

--- core/progress_test.go
+++ core/progress_test.go
@@ -1,6 +1,8 @@
 package core
 
 import (
+	"bytes"
+	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -19,6 +21,29 @@ func TestProgressReporter_ReachesOneHundred(t *testing.T) {
 	reporter.Add(10)
 
 	want := []int{30, 60, 100}
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("progress updates mismatch (-want +got):\n%s", diff)
+	}
+}
+
+func TestCopyWithProgress_CopiesBytesAndReportsProgress(t *testing.T) {
+	t.Parallel()
+
+	src := strings.NewReader("abcdefghij")
+	var dst bytes.Buffer
+	var got []int
+	reporter := newProgressReporter(10, func(progress int) {
+		got = append(got, progress)
+	})
+
+	if err := copyWithProgress(&dst, src, reporter); err != nil {
+		t.Fatalf("copyWithProgress() error = %v", err)
+	}
+	if diff := cmp.Diff("abcdefghij", dst.String()); diff != "" {
+		t.Errorf("copied contents mismatch (-want +got):\n%s", diff)
+	}
+
+	want := []int{100}
 	if diff := cmp.Diff(want, got); diff != "" {
 		t.Errorf("progress updates mismatch (-want +got):\n%s", diff)
 	}
