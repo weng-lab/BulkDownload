4a3f590b8e3a52a2ace92cdccfda7e0510d0532f
Jair Meza <jair3202@gmail.com>
2026-03-25 00:48:26 -0400

    simplify main bootstrap and shutdown flow

--- main.go
+++ main.go
@@ -25,28 +25,26 @@ const (
 	httpServerShutdownTimeout   = 10 * time.Second
 )
 
-var shutdownSignals = []os.Signal{os.Interrupt, syscall.SIGTERM}
-
-func newHTTPServer(config appconfig.Config, handler http.Handler) *http.Server {
-	return &http.Server{
-		Addr:              ":" + config.Port,
-		Handler:           handler,
-		ReadHeaderTimeout: httpServerReadHeaderTimeout,
-		ReadTimeout:       httpServerReadTimeout,
-		IdleTimeout:       httpServerIdleTimeout,
+var (
+	shutdownSignals = []os.Signal{os.Interrupt, syscall.SIGTERM}
+	notifyContext   = signal.NotifyContext
+	serveHTTPServer = func(server *http.Server) error {
+		return server.ListenAndServe()
 	}
-}
+	shutdownHTTPServer = func(server *http.Server, ctx context.Context) error {
+		return server.Shutdown(ctx)
+	}
+	startCleanup = service.StartCleanup
+)
 
-type lifecycleServer interface {
-	ListenAndServe() error
-	Shutdown(context.Context) error
-}
+func run(ctx context.Context, config appconfig.Config) error {
+	if err := os.MkdirAll(config.JobsDir, 0o755); err != nil {
+		return fmt.Errorf("create jobs dir: %w", err)
+	}
 
-func newShutdownContext(parent context.Context) (context.Context, context.CancelFunc) {
-	return signal.NotifyContext(parent, shutdownSignals...)
-}
-
-func serveUntilShutdown(ctx context.Context, server lifecycleServer, stopCleanup func()) error {
+	jobStore := jobs.NewJobs()
+	manager := service.NewManager(jobStore, config)
+	stopCleanup := startCleanup(jobStore, config.JobsDir, config.CleanupTick)
 	var stopOnce sync.Once
 	safeStopCleanup := func() {
 		if stopCleanup == nil {
@@ -56,9 +54,29 @@ func serveUntilShutdown(ctx context.Context, server lifecycleServer, stopCleanup
 	}
 	defer safeStopCleanup()
 
+	server := &http.Server{
+		Addr:              ":" + config.Port,
+		Handler:           api.NewRouter(manager, jobStore, config),
+		ReadHeaderTimeout: httpServerReadHeaderTimeout,
+		ReadTimeout:       httpServerReadTimeout,
+		IdleTimeout:       httpServerIdleTimeout,
+	}
+
+	log.Printf(
+		"config: jobs_dir=%s source_root_dir=%s public_base_url=%s download_root_dir=%s port=%s job_ttl=%s cleanup_tick=%s",
+		config.JobsDir,
+		config.SourceRootDir,
+		config.PublicBaseURL,
+		config.DownloadRootDir,
+		config.Port,
+		config.JobTTL,
+		config.CleanupTick,
+	)
+	log.Printf("bulk download service listening on :%s", config.Port)
+
 	serverErrCh := make(chan error, 1)
 	go func() {
-		err := server.ListenAndServe()
+		err := serveHTTPServer(server)
 		if errors.Is(err, http.ErrServerClosed) {
 			serverErrCh <- nil
 			return
@@ -75,7 +93,7 @@ func serveUntilShutdown(ctx context.Context, server lifecycleServer, stopCleanup
 		shutdownCtx, cancel := context.WithTimeout(context.Background(), httpServerShutdownTimeout)
 		defer cancel()
 
-		if err := server.Shutdown(shutdownCtx); err != nil {
+		if err := shutdownHTTPServer(server, shutdownCtx); err != nil {
 			return fmt.Errorf("shutdown server: %w", err)
 		}
 
@@ -83,37 +101,12 @@ func serveUntilShutdown(ctx context.Context, server lifecycleServer, stopCleanup
 	}
 }
 
-func run(ctx context.Context, config appconfig.Config) error {
-	if err := os.MkdirAll(config.JobsDir, 0o755); err != nil {
-		return fmt.Errorf("create jobs dir: %w", err)
-	}
-
-	jobStore := jobs.NewJobs()
-	manager := service.NewManager(jobStore, config)
-	stopCleanup := service.StartCleanup(jobStore, config.JobsDir, config.CleanupTick)
-	server := newHTTPServer(config, api.NewRouter(manager, jobStore, config))
-
-	log.Printf(
-		"config: jobs_dir=%s source_root_dir=%s public_base_url=%s download_root_dir=%s port=%s job_ttl=%s cleanup_tick=%s",
-		config.JobsDir,
-		config.SourceRootDir,
-		config.PublicBaseURL,
-		config.DownloadRootDir,
-		config.Port,
-		config.JobTTL,
-		config.CleanupTick,
-	)
-	log.Printf("bulk download service listening on :%s", config.Port)
-
-	return serveUntilShutdown(ctx, server, stopCleanup)
-}
-
 func main() {
 	config, err := appconfig.LoadConfig()
 	if err != nil {
 		log.Fatalf("load config: %v", err)
 	}
-	ctx, stop := newShutdownContext(context.Background())
+	ctx, stop := notifyContext(context.Background(), shutdownSignals...)
 	defer stop()
 
 	if err := run(ctx, config); err != nil {
